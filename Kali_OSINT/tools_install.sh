99cedf5123a087676833e683b51cd9f310fd1467
bikram <bikram@bikrams-MacBook-Pro.local>
2021-11-07 22:27:15 -0800

    Add Kali OSINT Vagrant

---
+++ Kali_OSINT/tools_install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Linux packages
+# sudo apt update;
+# sudo apt upgrade;
+# sudo apt-get update;
+# sudo apt-get upgrade;
+sudo apt install -y golang-go;
+sudo apt install -y terminator;
+sudo apt install -y gobuster;
+sudo apt install -y seclists;
+sudo apt install -y dnsenum;
+sudo apt install -y eyewitness;
+sudo apt install -y metagoofil;
+sudo apt install -y tor && apt install torbrowser-launcher;
+sudo apt install -y httrack;
+sudo apt install -y sherlock;
+sudo apt install -y keepassxc;
+
+# # Python packages
+# pip3 install --user --upgrade git+https://github.com/twintproject/twint.git#egg=twint;
+# pip3 install holehe;
+# pip3 install ignorant;
+# pip3 install h8mail;
+
+# mkdir Tools;
+# cd Tools;
+
+# Github packages
+# git clone https://github.com/HowToFind-bot/YaSeeker.git;
+# git clone https://github.com/Datalux/Osintgram.git;
+# git clone https://github.com/MrNonoss/Profil3r-docker.git;
+# git clone https://github.com/mxrch/GHunt.git;
+# git clone https://github.com/megadose/nqntnqnqmb.git;
+
+# Golang packages
+# go install -u github.com/tomnomnom/gf;
+
+# nmap cobalt strike nse
+
